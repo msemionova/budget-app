3db3d23006b996df516cc6199280c018adbdc1ba
Maria Semionova <maria.semionova@nnamu.com>
2024-08-10 12:44:57 +0200

    gh pages fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export', // Ensure static export is enabled
+  basePath: '/budget-app', // Set this to the subpath of your GitHub Pages site
+  assetPrefix: '/budget-app/', // Prefix for assets to ensure correct loading
+}
 
 export default nextConfig
