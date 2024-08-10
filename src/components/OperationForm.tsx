ce9492abd7f455c6a12e6283c96809dc8eaf6fa8
Maria Semionova <maria.semionova@nnamu.com>
2024-08-10 16:08:11 +0200

    fix icons

--- src/components/OperationForm.tsx
+++ src/components/OperationForm.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react'
 import type { OperationFormProps } from '@/lib/types'
 import { Button, Input } from './ui'
+import { Plus, Minus } from 'lucide-react'
 
 const OperationForm: React.FC<OperationFormProps> = ({
   onAddOperation,
@@ -23,7 +24,7 @@ const OperationForm: React.FC<OperationFormProps> = ({
     e.preventDefault()
     if (amount) {
       onAddOperation(Number(amount), description, type)
-      setAmount(0)
+      setAmount('')
       setDescription('')
       setType('income')
     }
@@ -31,37 +32,39 @@ const OperationForm: React.FC<OperationFormProps> = ({
 
   return (
     <form onSubmit={handleSubmit} className="flex flex-col gap-2 p-4 w-full">
-      <Input
-        type="number"
-        placeholder="Количество"
-        value={amount}
-        onChange={(e) => setAmount(Number(e.target.value))}
-        className="p-2 border rounded"
-        required
-      />
-      <Input
-        type="text"
-        placeholder="Описание"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-        className="p-2 border rounded"
-      />
-      <div className="flex justify-between w-full gap-5">
+      <div className="flex justify-between w-full gap-2">
+        <Input
+          type="number"
+          placeholder="Количество"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          className="p-2 border rounded"
+          required
+        />
+        <Input
+          type="text"
+          placeholder="Описание"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          className="p-2 border rounded"
+        />
+      </div>
+      <div className="flex justify-between w-full gap-2">
         {initialOperation ? (
           <>
             <Button
               onClick={() => setType('income')}
               type="submit"
-              className="p-2 bg-green-500 text-white rounded basis-1/2"
+              className="p-2 bg-green-500 text-white rounded"
             >
-              Изменить на: Пополнение
+              Изменить на: <Plus />
             </Button>
             <Button
               onClick={() => setType('expense')}
               type="submit"
-              className="p-2 bg-orange-500 text-white rounded basis-1/2"
+              className="p-2 bg-orange-500 text-white rounded"
             >
-              Изменить на: Расход
+              Изменить на: <Minus />
             </Button>
           </>
         ) : (
@@ -69,16 +72,16 @@ const OperationForm: React.FC<OperationFormProps> = ({
             <Button
               onClick={() => setType('income')}
               type="submit"
-              className="p-2 bg-green-500 text-white rounded basis-1/2"
+              className="p-2 bg-green-500 text-white rounded"
             >
-              Пополнение
+              <Plus />
             </Button>
             <Button
               onClick={() => setType('expense')}
               type="submit"
-              className="p-2 bg-orange-500 text-white rounded basis-1/2"
+              className="p-2 bg-orange-500 text-white rounded"
             >
-              Расход
+              <Minus />
             </Button>
           </>
         )}
